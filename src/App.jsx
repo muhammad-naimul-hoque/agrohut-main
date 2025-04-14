4edff65d09d3c2e78cdcab566a5ffae949f55c30
Muhammad Naimul Hoque <naimulhoq89@gmail.com>
2025-04-15 03:38:09 +0600

    navbar is added with log in/out

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
-      <div>
-          
-    </div>
-  )
-}
+    <>
+      <Navbar></Navbar>
+    </>
+  );
+};
 
-export default App
+export default App;
