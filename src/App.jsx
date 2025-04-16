2d6a2ae4dfcc3a2dc8ea29e4e0c49ae2968e2975
Muhammad Naimul Hoque <naimulhoq89@gmail.com>
2025-04-17 01:23:21 +0600

    login form is added

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,17 @@ import { Route, Routes, useLocation } from "react-router";
 import Home from "./pages/Home";
 import { Toaster } from "react-hot-toast";
 import Footer from "./components/Footer";
+import Login from "./components/Login";
+import { useAppContext } from "./context/AppContext";
 
 const App = () => {
   const isSellerPath = useLocation().pathname.includes("seller");
+  const { showUserLogin } = useAppContext();
 
   return (
     <div>
       {isSellerPath ? null : <Navbar />}
+      {showUserLogin ? <Login /> : null}
       <Toaster></Toaster>
       <div
         className={`${isSellerPath ? "" : "px-6 md:px-16 lg:px-24 xl:px-32"}`}
