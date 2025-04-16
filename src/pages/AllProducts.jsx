5ec6864308d739180cb1eb205de8d55f21e3f0dd
Muhammad Naimul Hoque <naimulhoq89@gmail.com>
2025-04-17 02:24:24 +0600

    all product page is added

---
+++ src/pages/AllProducts.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const AllProducts = () => {
+  return (
+    <div className="mt-16 flex flex-col">
+      <div className="flex flex-col items-end w-max">
+        <p class="text-2xl font-medium uppercase">All products</p>
+        <div class="w-16 h-0.5 bg-primary rounded-full"></div>
+      </div>
+    </div>
+  );
+};
+
+export default AllProducts;
