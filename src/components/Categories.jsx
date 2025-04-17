326b1368e6df302302c2270d1c236c8bc17da5a5
Muhammad Naimul Hoque <naimulhoq89@gmail.com>
2025-04-17 19:17:32 +0600

    categories navigation is set

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -15,7 +15,7 @@ const Categories = () => {
             className="group cursor-pointer py-5 px-3 gap-2 rounded-lg flex flex-col justify-center items-center"
             style={{ backgroundColor: category.bgColor }}
             onClick={() => {
-              navigate(`/product/${category.path.toLowerCase()}`);
+              navigate(`/products/${category.path.toLowerCase()}`);
               scrollTo(0, 0);
             }}
           >
