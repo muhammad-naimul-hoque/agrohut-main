5be9ea763da84f6c1fb4c74c8d643697410a207c
Muhammad Naimul Hoque <naimul.programmer@gmail.com>
2025-04-22 04:31:07 +0600

    add product is done & product list is added

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,9 @@ import AddAddress from "./pages/AddAddress";
 import MyOrders from "./pages/MyOrders";
 import SellerLogin from "./components/seller/SellerLogin";
 import SellerLayout from "./pages/seller/SellerLayout";
+import AddProduct from "./pages/seller/AddProduct";
+import ProductList from "./pages/seller/ProductList";
+import Orders from "./pages/seller/Orders";
 
 const App = () => {
   const isSellerPath = useLocation().pathname.includes("seller");
@@ -38,7 +41,11 @@ const App = () => {
           <Route
             path="/seller"
             element={isSeller ? <SellerLayout /> : <SellerLogin />}
-          ></Route>
+          >
+            <Route index element={isSeller ? <AddProduct /> : null} />
+            <Route path="product-list" element={<ProductList/>} />
+            <Route path="orders" element={<Orders/> } />
+          </Route>
         </Routes>
       </div>
       {!isSellerPath && <Footer />}
